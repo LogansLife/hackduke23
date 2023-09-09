e30df55a706696b37ef56c22ce4f0ffa9a634cf1
Justin Aronwald <jaronwald@gmail.com>
2023-09-09 12:51:59 -0400

    working homescreen

--- HackDuke/App.tsx
+++ HackDuke/App.tsx
@@ -1,61 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {SafeAreaView, useColorScheme, StatusBar, Text} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {enableScreens} from 'react-native-screens';
+import HomeScreen from './HomeScreen';
+enableScreens();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Sample screens for the bottom tabs
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function SettingsScreen() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <SafeAreaView>
+      <Text>Settings Screen</Text>
+    </SafeAreaView>
   );
 }
 
-function App(): JSX.Element {
+function FinanceScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Settings Screen</Text>
+    </SafeAreaView>
+  );
+}
+function PoliticsScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Settings Screen</Text>
+    </SafeAreaView>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -63,56 +42,21 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={backgroundStyle} flex={1}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen name="Finance" component={FinanceScreen} />
+          <Tab.Screen name="Politics" component={PoliticsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ HackDuke/HomeScreen.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+  Button,
+  Dimensions,
+  Image,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import {ScrollView} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+function HomeScreen({navigation}) {
+  return (
+    <>
+      <View>
+        <Text>Home Screen</Text>
+        <Image
+          source={require('./assets/bear-animal-flat-vector-design-isolated-free-png.webp')}
+          style={styles.bear}
+        />
+        <ScrollView style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Finance')}>
+            <Text style={styles.buttonText}>{'Finance'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Settings')}>
+            <Text style={styles.buttonText}>{'Settings'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Politics')}>
+            <Text style={styles.buttonText}>{'Politics'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Finance')}>
+            <Text style={styles.buttonText}>{'Finance'}</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </>
+  );
+}
+
+const styles = {
+  bear: {
+    width: windowWidth * 0.85,
+    height: windowHeight * 0.28,
+    left: windowWidth * 0.05,
+    top: windowHeight * 0.15,
+    marginBottom: windowHeight * 0.2,
+  },
+  button: {
+    width: windowWidth,
+    height: windowHeight * 0.05,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    borderRadius: 10,
+    marginBottom: windowHeight * 0.005,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  buttonContainer: {
+    top: windowHeight * 0,
+  },
+};
+
+export default HomeScreen;
